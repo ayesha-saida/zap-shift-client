116a1a1caab651d81545ee7ff389e408a277b3fe
ayesha-saida <ayeshasultana0305@gmail.com>
2025-12-05 01:47:54 +0600

    stores the tracking ID and transaction ID in state, and displays them

--- src/pages/Dashboard/payment/PaymentSuccess.jsx
+++ src/pages/Dashboard/payment/PaymentSuccess.jsx
@@ -1,24 +1,34 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router'
 import useAxiosSecure from '../../../components/useAxiosSecure'
 
 const PaymentSuccess = () => {
   const [searchParams] = useSearchParams()
+  const [paymentInfo, setPaymentInfo] = useState({})
   const sessionId = searchParams.get('session_id')
   console.log(sessionId)
   const axiosInstance = useAxiosSecure()
 
   useEffect(() => {
     if(sessionId){
-         axiosInstance.patch(`/payment-succes?session_id=${sessionId}`)
+         axiosInstance.patch(`/payment-success?session_id=${sessionId}`)
           .then(res => {
             console.log(res.data)
+
+           setPaymentInfo({
+               trackingId: res.data.trackingId,
+               transactionId: res.data.transactionId
+            })   
+
           })
     }
-  },[sessionId,axiosInstance])
+  }, [sessionId])
+
   return (
     <div>
         <h2 className='text-4xl font-semibold'>Payment Successful</h2>
+         <p>Your Transaction ID: {paymentInfo.transactionId}</p>
+         <p>Your Parcel Tracking ID: {paymentInfo.trackingId} </p> 
     </div>
   )
 }
