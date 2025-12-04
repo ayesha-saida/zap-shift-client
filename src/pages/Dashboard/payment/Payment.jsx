86c6032593f6767eaf9c0e7647a2653a4bf3475f
ayesha-saida <ayeshasultana0305@gmail.com>
2025-12-04 12:45:36 +0600

    In payment component data fetched from backend using tanstack query and a secure axios instance

--- src/pages/Dashboard/payment/Payment.jsx
+++ src/pages/Dashboard/payment/Payment.jsx
@@ -1,16 +1,30 @@
 import { useQuery } from '@tanstack/react-query'
 import React from 'react'
 import { useParams } from 'react-router'
+import useAxiosSecure from '../../../components/useAxiosSecure'
 
 const Payment = () => {
     const {parcelId} = useParams()
-    const {} = useQuery({
-        queryKey: ['parcels', parcelId]
+    const axiosInstance = useAxiosSecure()
+
+    const {isLoading, data: parcel} = useQuery({
+        queryKey: ['parcels', parcelId],
+        queryFn: async() => {
+            const res = await axiosInstance.get(`/parcels/${parcelId}`)
+             return res.data
+        }
     })
 
+     if(isLoading) {
+      return <div>
+        <span className="loading loading-dots loading-xl"></span>
+        </div>
+    }
+    
   return (
     <div>
-        <h2> Please Pay </h2>
+        <h2> Please Pay for: {parcel.parcelName}</h2>
+        <button className='btn btn-primary text-black'> Pay </button>
     </div>
   )
 }
