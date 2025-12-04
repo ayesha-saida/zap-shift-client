2e39a656e44204804d6551bad8d80bda5c77933d
ayesha-saida <ayeshasultana0305@gmail.com>
2025-12-04 21:57:14 +0600

    Update parcel info after payment is confirmed

--- src/pages/Dashboard/payment/PaymentSuccess.jsx
+++ src/pages/Dashboard/payment/PaymentSuccess.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSearchParams } from 'react-router'
+import useAxiosSecure from '../../../components/useAxiosSecure'
 
 const PaymentSuccess = () => {
+  const [searchParams] = useSearchParams()
+  const sessionId = searchParams.get('session_id')
+  console.log(sessionId)
+  const axiosInstance = useAxiosSecure()
+
+  useEffect(() => {
+    if(sessionId){
+         axiosInstance.patch(`/payment-succes?session_id=${sessionId}`)
+          .then(res => {
+            console.log(res.data)
+          })
+    }
+  },[sessionId,axiosInstance])
   return (
     <div>
         <h2 className='text-4xl font-semibold'>Payment Successful</h2>
