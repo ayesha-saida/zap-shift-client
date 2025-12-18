43e1aeb4422726f616a37174c5f4258631520c52
ayesha-saida <ayeshasultana0305@gmail.com>
2025-12-18 21:15:01 +0600

    Display payment time also in the payment history table

--- src/pages/Dashboard/payment/PaymentHistory.jsx
+++ src/pages/Dashboard/payment/PaymentHistory.jsx
@@ -26,6 +26,7 @@ const PaymentHistory = () => {
         <th></th>
         <th>Name</th>
         <th>Amount</th>
+        <th>Time</th>
         <th>Transaction</th>
       </tr>
     </thead>
@@ -36,6 +37,7 @@ const PaymentHistory = () => {
         <th>{index + 1}</th>
         <td>{user?.displayName}</td>
         <td>${payment.amount}</td>
+        <td>${payment.paidAt}</td>
         <td>{payment.transactionId}</td>
       </tr>)
       }
