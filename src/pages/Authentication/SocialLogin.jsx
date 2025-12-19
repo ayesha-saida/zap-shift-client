1d160a0ee4f71bc071112a02fb06fa0f42c2c7b0
ayesha-saida <ayeshasultana0305@gmail.com>
2025-12-19 18:39:28 +0600

    Store social user data in db

--- src/pages/Authentication/SocialLogin.jsx
+++ src/pages/Authentication/SocialLogin.jsx
@@ -3,9 +3,11 @@ import { useContext } from 'react'
 import { FcGoogle } from 'react-icons/fc'
 import { useLocation, useNavigate } from 'react-router'
 import { AuthContext } from '../../contexts/AuthContext'
+import useAxiosSecure from '../../components/useAxiosSecure'
 
 const SocialLogin = () => {
     const {signInGoogle} = useContext(AuthContext)
+    const axiosSecure = useAxiosSecure()
     const location = useLocation()
   //  console.log('location in social', location)
     const navigate = useNavigate()
@@ -15,7 +17,20 @@ const SocialLogin = () => {
       .then((result) => {
         console.log(result.user)
        alert("Successfully SignIn with Google");
-        navigate(location.state || '/' );
+
+         // create user in the database
+   const userInfo = {
+    email: result.user.email,
+    displayName: result.user.displayName ,
+    photoURL: result.user.photoURL
+   }
+
+   axiosSecure.post('/users', userInfo)
+   .then(res => {
+     console.log('user data has been stored', res.data)
+     navigate(location.state || '/' );
+   })
+
       })
       .catch((error) => {
        console.log(error)
