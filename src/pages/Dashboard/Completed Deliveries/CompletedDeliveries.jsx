7a5ecaad9fa203c85a40a7f0c65dc99f2c07f730
ayesha-saida <ayeshasultana0305@gmail.com>
2025-12-28 02:09:36 +0600

    display completed task on a table and calculate riders payment

--- src/pages/Dashboard/Completed Deliveries/CompletedDeliveries.jsx
+++ src/pages/Dashboard/Completed Deliveries/CompletedDeliveries.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../../contexts/AuthContext';
+import useAxiosSecure from '../../../components/Hooks/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
 
 const CompletedDeliveries = () => {
+      const { user } = useContext(AuthContext);
+    const axiosSecure = useAxiosSecure();
+
+    const { data: parcels = [], } = useQuery({
+        queryKey: ['parcels', user.email, 'driver_assigned'],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/parcels/rider?riderEmail=${user.email}&deliveryStatus=parcel_delivered`)
+
+            return res.data;
+        }
+    })
+
+    const calculatePayout = (parcel)  => {
+       if (parcel.senderDistrict === parcel.receiverDistrict) {
+            return parcel.cost * 0.8  //rider will get 80% from total charges
+        }
+        else{
+            return parcel.cost * 0.6;
+        }
+
+    }
+
   return (
     <div>
-        <h2 className='text-4xl'>Completed Deliveries: </h2>
+        <h2 className='text-4xl'>Completed Deliveries: {parcels.length} </h2>
+   
+   <div className="overflow-x-auto">
+                <table className="table table-zebra">
+                    {/* head */}
+                    <thead>
+                           <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Created At</th>
+                            <th>Pickup District</th>
+                            <th>Cost</th>
+                            <th>Payout</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                          {parcels.map((parcel, index) => <tr key={parcel._id}>
+                            <th>{index + 1}</th>
+                            <td>{parcel.parcelName}</td>
+                            <td>{parcel.createdAt}</td>
+                            <td>{parcel.senderDistrict}</td>
+                            <td>{parcel.cost}</td>
+                            <td>{calculatePayout(parcel)}</td>  
+                            <td>
+                                <button
+
+                                    className='btn btn-primary text-black'>Cash out</button>
+                            </td>
+                            
+                        </tr>)}
+
+                    </tbody>
+                          </table>
+            </div>
     </div>
   )
 }
